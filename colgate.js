53032c47df7687ee349cb082ce7f6bcfdd17c222
Adri833 <adrialvarez638@gmail.com>
2024-08-02 20:29:41 +0200

    Pasta de dientes terminado

---
+++ colgate.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Selecciona el campo de entrada
+    const input = document.getElementById('input-cuadro');
+    
+    // Función para verificar el valor del input
+    function checkInput() {
+        if (input.value === '1508') {
+            // Redirige a otro archivo HTML cuando el valor es '123'
+            window.location.href = 'index.html'; 
+        }
+    }
+
+    // Agrega un evento de escucha para detectar cambios en el campo de entrada
+    input.addEventListener('input', checkInput);
+});
